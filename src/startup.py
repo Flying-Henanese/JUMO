530b2c96f372ac31d2e35786b6c066d295648dda
ZHOU Shujian <flyinghenanese@gmail.com>
2025-07-11 09:53:55 +0800

    部署在开发测试环境，在.env文件中指定CUDA

--- src/startup.py
+++ src/startup.py
@@ -52,6 +52,7 @@ setup_logger()
 task_repository = TaskRepository()
 minio_tool = MinioConnection()
 pdf_processor = PDFProcessor(minio_tool=minio_tool, task_repository=task_repository)
-thread_pool = ThreadPoolExecutor(max_workers=int(os.getenv('MAX_CURRENT_WORKER', 1)))# worker的数量已经在线程池中完成了配置
+thread_pool = ThreadPoolExecutor(max_workers=int(os.getenv('MAX_CURRENT_WORKER', 1)))
+
 patch_batchanalyze_output_to_markdown()
 
