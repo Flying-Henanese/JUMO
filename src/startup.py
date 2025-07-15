ab9ec1661ac156d0525e8ce85b5ca50b76afb397
ZHOU Shujian <flyinghenanese@gmail.com>
2025-07-15 16:03:24 +0800

    refactor(processor): replace custom device wrapper with safe merge functions
    Add safe_merge_2_list_blocks and safe_merge_2_text_blocks to handle missing fields in text blocks
    Remove unused imports and custom_get_device wrapper

--- src/startup.py
+++ src/startup.py
@@ -15,23 +15,11 @@ from utils.logging import setup_logger
 from data.operation import TaskRepository
 from utils.minio_tool import MinioConnection
 from concurrent.futures import ThreadPoolExecutor
-from utils.workers_threading_pool import ThreadPoolSingleton
-# get_device 失控了以后再考虑
-# from wrapper.load_gpu import custom_get_device
-# import mineru.utils.config_reader as config_reader
-# config_reader.get_device = custom_get_device
-from wrapper.pdf_boost_patch import load_images_from_pdf as custom_load_images_from_pdf
-import mineru.utils.pdf_image_tools
-mineru.utils.pdf_image_tools.load_images_from_pdf = custom_load_images_from_pdf
-from wrapper.image_processing_boost import get_ocr_result_list_parallel
-import mineru.utils.ocr_utils
-mineru.utils.ocr_utils.get_ocr_result_list = get_ocr_result_list_parallel
-# 解决合并文本中缺失字段的问题
-from wrapper.merge_text import safe_merge_2_list_blocks, safe_merge_2_text_blocks
+from wrapper.merge_text import safe_merge_2_list_blocks,safe_merge_2_text_blocks
 import mineru.backend.pipeline.para_split
+# 应用猴子补丁
 mineru.backend.pipeline.para_split.__merge_2_list_blocks = safe_merge_2_list_blocks
 mineru.backend.pipeline.para_split.__merge_2_text_blocks = safe_merge_2_text_blocks
-
 from processor.pdf_processor import PDFProcessor
 from processor.converters.table_to_markdown import patch_batchanalyze_output_to_markdown    
 
