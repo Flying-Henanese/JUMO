46579b2ea2533dea7a91d2f9b2aac31135e04e91
ZHOU Shujian <flyinghenanese@gmail.com>
2025-07-29 16:11:35 +0800

    调整使用vlm模式的方法

--- src/processor/vlm_mode.py
+++ src/processor/vlm_mode.py
@@ -74,12 +74,11 @@ class PDFProcessor:
                 middle_json, infer_result = vlm_doc_analyze(                                     # ★
                     pdf_bytes,
                     image_writer=image_writer,      # 继续复用 FileBasedDataWriter
-                    backend="sglang-engine",        # ★ 关键切换点
+                    backend="sglang-engine"      # ★ 关键切换点
                     # 不需要 server_url
                     # lang=current_task.ocr_lang or "ch",
-                    formula_enable=current_task.formula_enabled,
-                    table_enable=current_task.table_enabled,
-                    parse_method="auto"
+                    # formula_enable=current_task.formula_enabled,
+                    # table_enable=current_task.table_enabled,
                 )
 
                 # 上传图片
